b54b74cd62ddc76694a73b7cc16dae2d40d3364a
Vlad Begunov <53581923+Shiker2032@users.noreply.github.com>
2023-03-29 15:10:32 +0300

    Update WorkingWithFunctions.cpp

--- WorkingWithFunctions.cpp
+++ WorkingWithFunctions.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include<string.h>
+
+using namespace std;
+
+// #1 Replace first and last positions of string with char
+string replaceStr(string str, char ch ) {
+	int leng = str.length();
+	str[0] = ch;
+	str[leng - 1] = ch;
+	return str;
+}
+
+
+// #2 Remove whitespaces from begining and end of the string
+string removeSpaces(string str)
+{
+	for (int i = 0; i < str.length(); i++)
+	{
+		if (str[i] == ' ')
+		{
+			str.erase(i, 1);
+			i--;
+		}
+	}
+	return str;
+}
 
 int main()
 {
-    
+
+	string str1 = "test1", str2 = "   test2    ";
+	char ch = 'a';
+	cout << replaceStr(str1, ch) << endl;
+	cout << removeSpaces(str2);
 }
